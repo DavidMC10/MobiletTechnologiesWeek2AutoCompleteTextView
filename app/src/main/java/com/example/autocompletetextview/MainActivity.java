44c8aac3a8c4463c9fee988f3fb7d925f48e9572
David <davidmcelhinney10@hotmail.com>
2019-10-01 21:02:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/autocompletetextview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.autocompletetextview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] presidents = {
+            "Dwight D. Eisenhower",
+            "John F. Kennedy",
+            "Lyndon B. Johnson",
+            "Richard Nixon",
+            "Gerald Ford",
+            "Jimmy Carter",
+            "Ronald Reagan",
+            "George H. W. Bush",
+            "Bill Clinton",
+            "George W. Bush",
+            "Barack Obama",
+            "Donald Trump"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, presidents);
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        textView.setThreshold(3);
+        textView.setAdapter(adapter);
+    }
+}
